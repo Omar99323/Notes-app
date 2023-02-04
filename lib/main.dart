304f3496f36c37716f82e29fa343d018ff07de75
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-02-04 14:05:12 +0200

    adding hive to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/pages/edit_note.dart';
 import 'package:notes_app/pages/homepage.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('Notes');
   runApp(const Notes());
 }
 
@@ -15,8 +18,8 @@ class Notes extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       routes: {
-        HomePage.id :(context) => const HomePage(),
-        EditNotePage.id :(context) => const EditNotePage(),
+        HomePage.id: (context) => const HomePage(),
+        EditNotePage.id: (context) => const EditNotePage(),
       },
       initialRoute: HomePage.id,
     );
