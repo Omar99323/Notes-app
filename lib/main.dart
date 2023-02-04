526e20a6fa5f8ced1d437da3d14848d0d2e6d830
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-02-04 16:34:13 +0200

    add cubit provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/cubits/addnote_cubit/addnote_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/pages/edit_note.dart';
 import 'package:notes_app/pages/homepage.dart';
@@ -16,14 +18,17 @@ class Notes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      routes: {
-        HomePage.id: (context) => const HomePage(),
-        EditNotePage.id: (context) => const EditNotePage(),
-      },
-      initialRoute: HomePage.id,
+    return BlocProvider(
+      create: (context) => AddnoteCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        routes: {
+          HomePage.id: (context) => const HomePage(),
+          EditNotePage.id: (context) => const EditNotePage(),
+        },
+        initialRoute: HomePage.id,
+      ),
     );
   }
 }
