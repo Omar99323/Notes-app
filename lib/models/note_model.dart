d04f555df8cfb35aec9f5898008b560055be2b54
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-02-04 14:01:31 +0200

    create typeadapter for note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,10 +1,17 @@
-import 'dart:ffi';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
 
-class NoteModel {
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
-  final Int color;
+  @HiveField(3)
+  final Color color;
 
   NoteModel(
       {required this.title,
