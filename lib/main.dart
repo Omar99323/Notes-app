82ab3e73812f32cfaba39f4331ff68d6b1e709a9
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-02-15 20:04:38 +0200

    add attributes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/cubits/addnote_cubit/addnote_cubit.dart';
@@ -22,6 +23,11 @@ class Notes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.black12,
+      ),
+    );
     return MultiBlocProvider(
       providers: [
         BlocProvider(
